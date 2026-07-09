03074d01fb23720bb3fe17831fa5723fd757a4f8
aarav-dev2007 <aaravone007@gmail.com>
2026-07-09 21:17:35 +0530

    Started Tic Tac Toe game structure

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function loadGame(game){
     const area = document.getElementById("game-area");
 
     if (game== "tictactoe") {
-        area.innerHTML="<h2>Tic Tac Toe coming soon</h2>";
+        area.innerHTML=`
+        <h2>Tic Tac Toe</h2>
+        <div id="board">
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>
+        <div class="cell" onclick="Turn(this)"></div>`;
     }
     else if (game == "snake") {
         area.innerHTML="<h2>Snake coming soon</h2>";
@@ -10,4 +21,9 @@ function loadGame(game){
     else if (game == 'rps') {
         area.innerHTML="<h2>Rock Paper Scissors coming soon</h2>"
     }
+}
+
+
+function Turn(cell) {
+    cell.innerText = "X";
 }
