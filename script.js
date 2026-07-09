b7edfb28491f7b0ed63db4d475df3a3bc29fc879
aarav-dev2007 <aaravone007@gmail.com>
2026-07-09 20:43:38 +0530

    Basic HTML, CSS, JS setup

---
+++ script.js
@@ -0,0 +1,13 @@
+function loadGame(game){
+    const area = document.getElementById("game-area");
+
+    if (game== "tictactoe") {
+        area.innerHTML="<h2>Tic Tac Toe coming soon</h2>";
+    }
+    else if (game == "snake") {
+        area.innerHTML="<h2>Snake coming soon</h2>";
+    }
+    else if (game == 'rps') {
+        area.innerHTML="<h2>Rock Paper Scissors coming soon</h2>"
+    }
+}
