0173d7cd339f61e3bfc3ce24d285bae190733717
aarav-dev2007 <aaravone007@gmail.com>
2026-07-10 15:39:40 +0530

    Added Restart Button

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let current='X';
+let end=false;
+
 
 function loadGame(game){
+    end=false;
+    current='X';
     const area = document.getElementById("game-area");
 
     if (game== "tictactoe") {
         area.innerHTML=`
         <h2>Tic Tac Toe</h2>
+        <h3 id="status"></h3>
         <div id="board">
         <div class="cell" onclick="Turn(this)"></div>
         <div class="cell" onclick="Turn(this)"></div>
@@ -15,7 +20,11 @@ function loadGame(game){
         <div class="cell" onclick="Turn(this)"></div>
         <div class="cell" onclick="Turn(this)"></div>
         <div class="cell" onclick="Turn(this)"></div>
-        <div class="cell" onclick="Turn(this)"></div></div>`;
+        <div class="cell" onclick="Turn(this)"></div>
+        </div>
+        <button class="restart-btn" onclick='restart()'>
+        Restart</button>`;
+        document.getElementById("status").innerText="X's Turn";
     }
     else if (game == "snake") {
         area.innerHTML="<h2>Snake coming soon</h2>";
@@ -28,6 +37,7 @@ function loadGame(game){
 
 
 function Winner(){
+    const status=document.getElementById("status");
     const cells=document.querySelectorAll(".cell");
     const combos=[
         [0,1,2],[3,4,5],[6,7,8], 
@@ -43,7 +53,7 @@ function Winner(){
             cells[a].innerText === cells[b].innerText &&
             cells[a].innerText === cells[c].innerText
         ){
-            alert(cells[a].innerText + " Wins!");
+            status.innerText = cells[a].innerText + " Wins!";
             return true;
         }
     }
@@ -52,6 +62,11 @@ function Winner(){
 
 
 function Turn(cell){
+
+    if(end) {
+        return;
+    }
+
     if (cell.innerText !== ""){
         return;
     }
@@ -59,6 +74,7 @@ function Turn(cell){
     cell.innerText=current;
 
     if (Winner()){
+        end=true;
         return;
     }
 
@@ -68,4 +84,17 @@ function Turn(cell){
     else{
         current='X'
     }
+}
+
+function restart(){
+    const cells = document.querySelectorAll(".cell");
+    const status= document.getElementById("status");
+
+    for (let i=0; i<cells.length; i= i+1){
+        cells[i].innerText='';
+    }
+
+    current = 'X';
+    end = false;
+    status.innerText = "X's Turn";
 }
